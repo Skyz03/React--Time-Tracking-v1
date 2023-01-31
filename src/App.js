e03ac47a0e87ae292cdd622105635812ff90d7c8
Skyz03 <aakibdiscoo@gmail.com>
2023-01-31 21:23:07 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,66 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Report for
+      Jeremy Robson
+
+      Daily
+      Weekly
+      Monthly
+
+      Work
+      5hrs
+      Previous - 7hrs
+      32hrs
+      Previous - 36hrs
+      103hrs
+      Previous - 128hrs
+
+      Play
+      1hr
+      Previous - 2hrs
+      10hrs
+      Previous - 8hrs
+      23hrs
+      Previous - 29hrs
+
+      Study
+      0hrs
+      Previous - 1hr
+      4hrs
+      Previous - 7hrs
+      13hrs
+      Previous - 19hrs
+
+      Exercise
+      1hr
+      Previous - 1hr
+      4hrs
+      Previous - 5hrs
+      11hrs
+      Previous - 18hrs
+
+      Social
+      1hr
+      Previous - 3hrs
+      5hrs
+      Previous - 10hrs
+      21hrs
+      Previous - 23hrs
+
+      Self Care
+      0hrs
+      Previous - 1hr
+      2hrs
+      Previous - 2hrs
+      7hrs
+      Previous - 11hrs
+
+
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
     </div>
   );
 }
